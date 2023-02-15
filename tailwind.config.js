03e2ce9ef9f704ba9166a2c44cad742eb7796cf2
Kleine <renatasgal420@gmail.com>
2023-02-15 17:25:11 +0200

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+/* eslint-env node */
+module.exports = {
+  content: ['./src/**/*.{tsx,ts}', './index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
